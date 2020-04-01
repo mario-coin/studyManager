9a9fc201960669fbb9943a947798c9ad0e64ea29
Mario Coin Vargas Junior <mario@luedtke.com.br>
2020-04-01 01:43:28 -0300

    built orm sequelize

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { User } = require('../client/models');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/mensagem', (req, res) => {
+  User.create({ name: 'Mario Coin', email: 'marioquercafe@gmail.com', password: '123456' });
   res.send({ express: 'let the games begin' });
 });
 
