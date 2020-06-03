d6b7fd5a01ed2191086f820d0256b0cb7ab84eff
Mario Coin Vargas Junior <mario@luedtke.com.br>
2020-06-02 23:51:30 -0300

    usuário padrao para banco sql e atualização do readme

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  username: 'root',
-  password: 'dol blathanna&15',
+  username: 'study_manager',
+  password: null,
   database: 'study_manager',
   host: '127.0.0.1',
   dialect: 'mysql',
