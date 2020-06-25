3e07101814af14ad170f32ea02b091c72e2e4bee
Patrick Hammes <patrick_hammes@hotmail.com>
2020-06-25 09:34:55 -0300

    alteracao conta mailtrap

--- server/modules/mailer.js
+++ server/modules/mailer.js
@@ -4,12 +4,13 @@ const transport = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "882b9123734e80",
-      pass: "0aafe73ebfb012"
+      user: "090805c42ed69b",
+      pass: "011d99badf6d28"
     }
   });
 
 module.exports = transport;
 
-//sstudymanager@outlook.com
+//conta no mailtrap
+//timeplagio@outlook.com
 //studymanager2020
